1ff1f4d51d992a45308fb2767cb8bf45290d3b88
Gareth Sears <garethsears@gmail.com>
2020-07-15 13:47:20 +0100

    Add CourseController functional test

---
+++ tests/CourseControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests;
+
+use App\DataFixtures\AppFixtures;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class CourseControllerTest extends WebTestCase
+{
+    public function testUnauthorisedUsersRedirected() {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/courses');
+        $this->assertResponseRedirects('/login', 301);
+    }
+}
