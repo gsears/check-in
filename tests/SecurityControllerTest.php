7538b4df8a2ee7abcdc7e49c3e618a21ac6eb0cc
Gareth Sears <garethsears@gmail.com>
2020-07-15 13:38:51 +0100

    Add login functional tests

---
+++ tests/SecurityControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Tests;
+
+use App\DataFixtures\AppFixtures;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\UrlHelper;
+
+class SecurityControllerTest extends WebTestCase
+{
+    public function usernameProvider() {
+        yield [AppFixtures::TEST_STUDENT_USERNAME];
+        yield [AppFixtures::TEST_INSTUCTOR_USERNAME];
+    }
+
+    /**
+     * @dataProvider usernameProvider
+     *
+     * Assert that users are redirected to their courses page on login
+     */
+    public function testLoginRedirect(string $username)
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+
+        $form = $crawler->selectButton('Sign in')->form();
+
+        $client->submit($form, [
+            'email' => $username,
+            'password' => 'password',
+        ]);
+
+        $this->assertSame(302, $client->getResponse()->getStatusCode());
+        $this->assertRegExp('/\/courses$/', $client->getResponse()->headers->get('Location'));
+    }
+}
