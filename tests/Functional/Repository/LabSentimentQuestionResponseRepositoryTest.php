69a96c39db265ec73fe6931790aa2f549e8caffa
Gareth Sears <garethsears@gmail.com>
2020-08-16 20:06:32 +0100

    Add LabSentimentQuestionResponseRepositoryTest

---
+++ tests/Functional/Repository/LabSentimentQuestionResponseRepositoryTest.php
@@ -0,0 +1,175 @@
+<?php
+
+/*
+LabSentimentQuestionResponseRepositoryTest.php
+Gareth Sears - 2493194S
+*/
+
+namespace App\Tests\Functional\Repository;
+
+use App\Containers\SurveyQuestionResponseRisk;
+use App\Containers\XYCoordinates;
+use App\Entity\LabSentimentQuestionResponse;
+use App\Entity\LabXYQuestionResponse;
+use App\Entity\SentimentQuestion;
+use App\Tests\Functional\FunctionalTestCase;
+use DateTime;
+
+class LabSentimentQuestionResponseRepositoryTest extends FunctionalTestCase
+{
+
+    public function testGetSurveyQuestionResponseRiskNotInDangerZoneBounds()
+    {
+        $creator = $this->getEntityCreator();
+
+        $course = $creator->createCourse(
+            'testCourse',
+            'test',
+            null
+        );
+
+        $courseInstance = $creator->createCourseInstance(
+            $course,
+            new DateTime("20 November 2020"),
+            new DateTime("21 November 2020")
+        );
+
+        $student = $creator->createStudent(
+            'test',
+            'test',
+            '1234'
+        );
+
+        $enrolment = $creator->createEnrolment(
+            $student,
+            $courseInstance
+        );
+
+        $lab = $creator->createLab(
+            'testLab',
+            new DateTime("20 November 2020"),
+            $courseInstance
+        );
+
+        $labResponse = $creator->createLabResponse(
+            true,
+            $student,
+            $lab
+        );
+
+        $sentimentQuestion = $creator->createSentimentQuestion(
+            'testSentimentQuestion',
+            'test'
+        );
+
+        $labSentimentQuestion = $creator->createLabSentimentQuestion(
+            0,
+            $sentimentQuestion,
+            $lab
+        );
+
+        $labXYQuestionResponse = $creator->createLabSentimentQuestionResponse(
+            'test response',
+            SentimentQuestion::NEUTRAL,
+            0.9,
+            $labSentimentQuestion,
+            $labResponse
+        );
+
+        /**
+         * @var LabXYQuestionResponseRepository
+         */
+        $repo = $this->getEntityManager()->getRepository(LabXYQuestionResponse::class);
+        $surveyQuestionResponseRisk = $repo->getSurveyQuestionResponseRisk($labXYQuestionResponse);
+
+        $this->assertNotNull($surveyQuestionResponseRisk);
+        $this->assertEquals(SurveyQuestionResponseRisk::LEVEL_NONE, $surveyQuestionResponseRisk->getRiskLevel());
+        $this->assertEquals($labXYQuestionResponse, $surveyQuestionResponseRisk->getSurveyQuestionResponse());
+    }
+
+    public function dangerZoneLevelProvider()
+    {
+        yield [SurveyQuestionResponseRisk::LEVEL_WARNING];
+        yield [SurveyQuestionResponseRisk::LEVEL_DANGER];
+    }
+
+    /**
+     * @dataProvider dangerZoneLevelProvider
+     */
+    public function testGetSurveyQuestionResponseRiskInWarningBounds(int $riskLevel)
+    {
+        $creator = $this->getEntityCreator();
+
+        $course = $creator->createCourse(
+            'testCourse',
+            'test',
+            null
+        );
+
+        $courseInstance = $creator->createCourseInstance(
+            $course,
+            new DateTime("20 November 2020"),
+            new DateTime("21 November 2020")
+        );
+
+        $student = $creator->createStudent(
+            'test',
+            'test',
+            '1234'
+        );
+
+        $enrolment = $creator->createEnrolment(
+            $student,
+            $courseInstance
+        );
+
+        $lab = $creator->createLab(
+            'testLab',
+            new DateTime("20 November 2020"),
+            $courseInstance
+        );
+
+        $labResponse = $creator->createLabResponse(
+            true,
+            $student,
+            $lab
+        );
+
+        $sentimentQuestion = $creator->createSentimentQuestion(
+            'testSentimentQuestion',
+            'test'
+        );
+
+        $labSentimentQuestion = $creator->createLabSentimentQuestion(
+            0,
+            $sentimentQuestion,
+            $lab
+        );
+
+        $labSentimentQuestionDangerZone = $creator->createLabSentimentQuestionDangerZone(
+            $riskLevel,
+            SentimentQuestion::NEGATIVE,
+            0.8,
+            1.0,
+            $labSentimentQuestion
+        );
+
+        $labXYQuestionResponse = $creator->createLabSentimentQuestionResponse(
+            'test response',
+            SentimentQuestion::NEGATIVE,
+            0.9,
+            $labSentimentQuestion,
+            $labResponse
+        );
+
+        /**
+         * @var LabSentimentQuestionResponseRepository
+         */
+        $repo = $this->getEntityManager()->getRepository(LabSentimentQuestionResponse::class);
+        $surveyQuestionResponseRisk = $repo->getSurveyQuestionResponseRisk($labXYQuestionResponse);
+
+        $this->assertNotNull($surveyQuestionResponseRisk);
+        $this->assertEquals($riskLevel, $surveyQuestionResponseRisk->getRiskLevel());
+        $this->assertEquals($labXYQuestionResponse, $surveyQuestionResponseRisk->getSurveyQuestionResponse());
+    }
+}
