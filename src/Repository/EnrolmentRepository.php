85072ab830262e139cc8006d987789b67ec705c1
Gareth Sears <garethsears@gmail.com>
2020-08-16 16:28:11 +0100

    Comment EnrolmentRepository

--- src/Repository/EnrolmentRepository.php
+++ src/Repository/EnrolmentRepository.php
@@ -1,11 +1,18 @@
 <?php
 
+/*
+EnrolmentRepository.php
+Gareth Sears - 2493194S
+*/
+
 namespace App\Repository;
 
 use App\Entity\Enrolment;
 use App\Entity\CourseInstance;
 use App\Containers\EnrolmentRisk;
+use App\Entity\LabResponse;
 use App\Provider\DateTimeProvider;
+use DateTime;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
@@ -17,22 +24,32 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
  */
 class EnrolmentRepository extends ServiceEntityRepository
 {
-
-    private $labResponseRepo;
-
-    public function __construct(ManagerRegistry $registry, LabResponseRepository $labResponseRepo)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Enrolment::class);
-        $this->labResponseRepo = $labResponseRepo;
     }
 
-    public function findEnrolmentRisksByCourseInstance(CourseInstance $courseInstance, bool $onlyAtRisk = false)
+    /**
+     * This is the main function for getting students at risk for a particular course instance.
+     *
+     * @param CourseInstance $courseInstance
+     * @param boolean $onlyAtRisk
+     * @param DateTime $currentTime
+     * @return EnrolmentRisk[] A collection of enrolment risk wrapper objects which contain risk information
+     * as well as student and enrolment information.
+     */
+    public function findEnrolmentRisksByCourseInstance(CourseInstance $courseInstance, bool $onlyAtRisk = false, DateTime $currentTime = null): array
     {
-        $currentTime = (new DateTimeProvider)->getCurrentDateTime();
+        // If no time is provided, set the current date time.
+        if (!$currentTime) {
+            $currentTime = (new DateTimeProvider)->getCurrentDateTime();
+        }
+
+        // Get the risk settings for the course instance
         $riskThreshold = $courseInstance->getRiskThreshold();
         $consecutiveLabCount = $courseInstance->getRiskConsecutiveLabCount();
 
-        // Get previous X labs from date
+        // Get previous X labs from date, according to consecutive lab count.
         $query = $this->getEntityManager()->createQuery('
             SELECT l FROM App\Entity\Lab l
             JOIN l.courseInstance ci
@@ -50,6 +67,7 @@ class EnrolmentRepository extends ServiceEntityRepository
             return [];
         }
 
+        // Get the student responses for each of the labs returned above
         $query = $this->getEntityManager()->createQuery('
             SELECT lr FROM App\Entity\LabResponse lr
             WHERE lr.lab IN (:labs)
@@ -58,28 +76,45 @@ class EnrolmentRepository extends ServiceEntityRepository
         $query->setParameter('labs', $labs);
         $labResponses = $query->getResult();
 
+        // Split the response so each student's responses are chunked together
         $responsesByStudent = array_chunk($labResponses, $consecutiveLabCount);
 
-        $enrolmentRisks = array_map(function ($responseChunk) use ($courseInstance) {
-            $labResponseRisks = array_map(function ($response) {
-                return $this->labResponseRepo->getLabResponseRisk($response);
+        /**
+         * @var LabResponseRepository
+         */
+        $labResponseRepo = $this->getEntityManager()->getRepository(LabResponse::class);
+
+        // Get the enrolment risks.
+        // For every student's response set...
+        $enrolmentRisks = array_map(function ($responseChunk) use ($courseInstance, $labResponseRepo) {
+            // Get the labResponseRisk object
+            $labResponseRisks = array_map(function ($response) use ($labResponseRepo) {
+                return $labResponseRepo->getLabResponseRisk($response);
             }, $responseChunk);
 
+            // Get the student and the enrolment for that student...
             $student = $responseChunk[0]->getStudent();
             $enrolment = $this->findOneBy([
                 'student' => $student,
                 'courseInstance' => $courseInstance
             ]);
 
+            // And wrap them in a container for further processing.
             return new EnrolmentRisk($labResponseRisks, $enrolment);
         }, $responsesByStudent);
 
+        // If the only at risk parameter is passed, filter out students who are not at risk according
+        // to the risk threshold configuration.
         if ($onlyAtRisk) {
-            $enrolmentRisks = array_filter($enrolmentRisks, function (EnrolmentRisk $enrolmentRisk) use ($riskThreshold) {
-                return $enrolmentRisk->areAllRisksAbove($riskThreshold);
-            });
+            $enrolmentRisks = array_filter(
+                $enrolmentRisks,
+                function (EnrolmentRisk $enrolmentRisk) use ($riskThreshold) {
+                    return $enrolmentRisk->areAllRisksAbove($riskThreshold);
+                }
+            );
         }
 
+        // Sort the results by average risk.
         EnrolmentRisk::sortByAverageRisk($enrolmentRisks);
 
         return $enrolmentRisks;
