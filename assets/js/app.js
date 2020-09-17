a0540d01098f9d2a8780c02336c4a4120e417823
Gareth Sears <garethsears@gmail.com>
2020-09-17 19:45:19 +0100

    Add loading UI for slow page loads

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,31 @@ import "./boostrap"; // Bootstrap import
 import "./tour"; // Shepherd js tour library import
 import "./tables"; // Tablesort library and methods.
 import "./xy"; // Custom XY component and Vue libraries
+
+/**
+ * Add loading UI for lengthy serverside database operations / external api calls.
+ */
+
+const displayLoadingBar = () => {
+  const loadingBar = document.getElementById("loading-bar");
+  loadingBar.classList.remove("d-none");
+};
+
+// On a link click.
+const links = Array.from(document.querySelectorAll("a"));
+links.forEach((l) => {
+  l.onclick = displayLoadingBar;
+});
+
+// On a submit click.
+const submitButtons = Array.from(
+  document.querySelectorAll('button[type="submit"]')
+);
+submitButtons.forEach((b) => {
+  b.onclick = function() {
+    submitButtons.forEach((b) => b.classList.add("disabled"));
+    this.innerHTML =
+      '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...';
+    displayLoadingBar();
+  };
+});
